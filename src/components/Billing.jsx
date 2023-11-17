4b64724c79489d9e4ab960e1a4608b50374c0024
anildegirmenci <anildegirmenci71@gmail.com>
2023-11-18 02:15:49 +0300

    Some components added.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {apple, bill, google} from '../assets'
+import styles, {layout} from '../style'
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billin" className='w-[100%] h-[100%] relative z-[5]' />
+    <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full
+    white__gradient"></div>
+    <div className="absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full
+    pink__gradient"></div>
+    </div>
+  </section>
+)
 
 export default Billing
